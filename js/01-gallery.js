8e088e6c8285fd9775d29a4852f6bce8fefc5bb3
vitalij99 <vitalisklymko@gmail.com>
2023-01-05 16:02:55 +0200

    first work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.getElementsByClassName("gallery")[0]
+let instance
+
+createImages(galleryItems)
+
+gallery.addEventListener("click", createModalImg)
+gallery.addEventListener("keydown", delateModalImg)
+
+function createImages(e) {
+   const images = e.reduce ((arr,{ preview, original, description }) => {
+       return arr + `<a class="gallery__link" href="${original}"><img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    /></a> ` 
+   },"")
+    gallery.insertAdjacentHTML('beforeend', images)    
+}
+
+
+function delateModalImg({key}) {    
+    if (key === "Escape") {        
+        instance.close()
+    }
+}
+
+function createModalImg(e) {    
+    e.preventDefault()
+     instance = basicLightbox.create(`<img src="${e.target.dataset.source}" >`)
+    instance.show() 
+        
+}
