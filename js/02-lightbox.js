a6a1d1c70c1de45a56803e74e44e06fb7a47863b
vitalij99 <vitalisklymko@gmail.com>
2023-01-07 13:52:07 +0200

    work2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.getElementsByClassName("gallery")[0]
+
+
+createImages(galleryItems)
+
+// gallery.addEventListener("click", createModalImg)
+// gallery.addEventListener("keydown", delateModalImg)
+
+function createImages(e) {
+   const images = e.reduce ((arr,{ preview, original, description }) => {
+       return arr + `<li><a class="gallery__item" href="${original}"><img class="gallery__image"
+      src="${preview}" alt="${description}"/></a></li> ` 
+   },"")
+    gallery.insertAdjacentHTML('beforeend', images)    
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+    captionsData: "alt",
+    captionDelay:250,
+});
